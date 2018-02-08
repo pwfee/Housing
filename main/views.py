322e69a51142b43c23962104aac2bebae59b8ebc
pwfee <vinsonring@gmail.com>
2018-02-08 15:11:07 +0800

    Beta 0.0.1

---
+++ main/views.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from sklearn.externals import joblib
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def predict(request):
+    model = joblib.load(BASE_DIR + '/static/model.pkl')
+    client_data = []  # Client 3
+
+    client_data.append(request.GET.get('area', '1'))
+    client_data.append(request.GET.get('room', '1'))
+    client_data.append(request.GET.get('living', '1'))
+
+    is_school = request.GET.get('school', 'false')
+
+    if is_school:
+        is_school = 1
+    else:
+        is_school = 0
+
+    client_data.append(is_school)
+
+    client_data.append(request.GET.get('year', '1'))
+
+    client_data.append(request.GET.get('floor', '1'))
+
+
+    str = model.predict(client_data)
+
+    return HttpResponse(str)
